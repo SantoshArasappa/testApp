caba39eee6f2f80385f99fca6e1bbf7b660eec19
Ravi D <jayaravi10@gmail.com>
2017-05-01 23:03:36 -0400

    added new files and script change

--- script.js
+++ script.js
@@ -394,57 +394,57 @@ angular.module('app', ['ngDropdowns', 'ngAnimate', 'ngSanitize', 'ui.bootstrap',
 $scope.gameFileListNew = 
 [
 	{
-		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/master/Games/India/Cricket",
+		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/TestEnv/Games/India/Cricket",
 		value: "Indian_Premier_League.ics"
 
 	},
 	
 	{
-		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/master/Games/Spain/Football",
+		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/TestEnv/Games/Spain/Football",
 		value: "La_Liga.ics"
 
 	},
 	
 	{
-		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/master/Games/West_Indies/Cricket",
+		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/TestEnv/Games/West_Indies/Cricket",
 		value: "Pakistan_Tour_of_West_Indies.ics"
 	},
     {
-		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/master/Games/England/Football",
+		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/TestEnv/Games/England/Football",
 		value: "FA_Cup.ics"
 	}, 
     {
-		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/master/Games/Italy/Football",
+		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/TestEnv/Games/Italy/Football",
 		value: "Serie_A.ics"
 	},
 	
 	{
-		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/master/Games/Multi/Football",
+		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/TestEnv/Games/Multi/Football",
 		value: "English_Premier_League.ics"
 
 	},
     
     {
-		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/master/Games/France/Tennis",
+		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/TestEnv/Games/France/Tennis",
 		value: "French_Open.ics"
 
 	},
 	
 	{
-		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/master/Games/England/Tennis",
+		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/TestEnv/Games/England/Tennis",
 		value: "Wimbledon.ics"
 	},
     {
-		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/master/Games/Georgia/Golf",
+		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/TestEnv/Games/Georgia/Golf",
 		value: "Golf_Masters_Tournament.ics"
 	}, 
     {
-		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/master/Games/England/Golf",
+		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/TestEnv/Games/England/Golf",
 		value: "Golf_The_Open_Championship.ics"
 	},
 	
 	{
-		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/master/Games/France/Golf",
+		url: "https://raw.githubusercontent.com/SantoshArasappa/testApp/TestEnv/Games/France/Golf",
 		value: "Golf_Ryder_Cup.ics"
 
 	}
